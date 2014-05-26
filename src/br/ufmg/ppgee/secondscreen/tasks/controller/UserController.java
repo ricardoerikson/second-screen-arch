eb11e83b4484c2f8bbb224cd52ca80941b92b458
Ricardo Erikson <ricardoerikson@gmail.com>
2014-05-26 14:25:39 -0400

    Spring MCV configured

--- src/br/ufmg/ppgee/secondscreen/tasks/controller/UserController.java
+++ src/br/ufmg/ppgee/secondscreen/tasks/controller/UserController.java
@@ -1,8 +1,28 @@
 package br.ufmg.ppgee.secondscreen.tasks.controller;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.googlecode.objectify.ObjectifyService;
+
+import br.ufmg.ppgee.secondscreen.platform.entities.User;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
+
+	static {
+		ObjectifyService.register(User.class);
+	}
+
+	@RequestMapping("create")
+	public void createUser(User user, HttpServletResponse response) {
+		ofy().save().entity(user);
+		response.setStatus(200);
+	}
 
 }
