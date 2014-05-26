7c6bef601eb58a16796bc57dbeca25b4b492157d
Ricardo Erikson <ricardoerikson@gmail.com>
2014-05-26 15:58:08 -0400

    Adding new users

--- src/br/ufmg/ppgee/secondscreen/controller/UserController.java
+++ src/br/ufmg/ppgee/secondscreen/controller/UserController.java
@@ -2,8 +2,6 @@ package br.ufmg.ppgee.secondscreen.controller;
 
 import static br.ufmg.ppgee.secondscreen.platform.objectify.OfyService.ofy;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +11,10 @@ import br.ufmg.ppgee.secondscreen.platform.entities.User;
 @RequestMapping("user")
 public class UserController {
 
-	@RequestMapping("create")
-	public void createUser(User user, HttpServletResponse response) {
+	@RequestMapping("new")
+	public String createUser(User user) {
 		ofy().save().entity(user);
-		response.setStatus(200);
+		return "saved";
 	}
 
 }
