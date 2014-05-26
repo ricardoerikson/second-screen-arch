078e1e1d116e816efa057aac8d344f3a8c1804a7
Ricardo Erikson <ricardoerikson@gmail.com>
2014-05-26 14:50:39 -0400

    OfyService class created

---
+++ src/br/ufmg/ppgee/secondscreen/platform/objectify/OfyService.java
@@ -0,0 +1,23 @@
+package br.ufmg.ppgee.secondscreen.platform.objectify;
+
+import br.ufmg.ppgee.secondscreen.platform.entities.User;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+public class OfyService {
+
+	static {
+		factory().register(User.class);
+	}
+
+	public static Objectify ofy() {
+		return ObjectifyService.ofy();
+	}
+
+	public static ObjectifyFactory factory() {
+		return ObjectifyService.factory();
+	}
+
+}
--- src/br/ufmg/ppgee/secondscreen/tasks/controller/UserController.java
+++ src/br/ufmg/ppgee/secondscreen/tasks/controller/UserController.java
@@ -1,23 +1,17 @@
 package br.ufmg.ppgee.secondscreen.tasks.controller;
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
+import static br.ufmg.ppgee.secondscreen.platform.objectify.OfyService.ofy;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.googlecode.objectify.ObjectifyService;
-
 import br.ufmg.ppgee.secondscreen.platform.entities.User;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-
-	static {
-		ObjectifyService.register(User.class);
-	}
 
 	@RequestMapping("create")
 	public void createUser(User user, HttpServletResponse response) {
