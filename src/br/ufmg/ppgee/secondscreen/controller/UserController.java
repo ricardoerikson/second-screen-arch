54bd910a7075436154318b7b1cf419c32fce1477
Ricardo Erikson <ricardoerikson@gmail.com>
2014-05-26 23:30:46 -0400

    Listing registered users

--- src/br/ufmg/ppgee/secondscreen/controller/UserController.java
+++ src/br/ufmg/ppgee/secondscreen/controller/UserController.java
@@ -2,19 +2,36 @@ package br.ufmg.ppgee.secondscreen.controller;
 
 import static br.ufmg.ppgee.secondscreen.platform.objectify.OfyService.ofy;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ufmg.ppgee.secondscreen.platform.entities.User;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("new")
-	public String createUser(User user) {
-		ofy().save().entity(user);
-		return "saved";
-	}
+    @RequestMapping("/new")
+    public String createUser(User user) {
+	ofy().save().entity(user);
+	return "saved";
+    }
+
+    @RequestMapping("/form")
+    public String getForm() {
+	return "user/userForm";
+    }
+
+    @RequestMapping("/list")
+    public String listUsers(Model model) {
+	List<User> list = ofy().load().type(User.class).list();
+	for (User u : list)
+	    System.out.println(u.getId() + ", " + u.getUsername());
+	model.addAttribute("users", list);
+	return "user/listusers";
+    }
 
 }
