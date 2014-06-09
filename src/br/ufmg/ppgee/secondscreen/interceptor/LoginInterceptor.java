20919aa6c77c0367dd59c14bb87a7bad9e26536a
Ricardo Erikson <ricardoerikson@gmail.com>
2014-06-08 21:04:27 -0400

    Login interceptor added to the project

---
+++ src/br/ufmg/ppgee/secondscreen/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package br.ufmg.ppgee.secondscreen.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+	    HttpServletResponse response, Object handler) throws Exception {
+	HttpSession session = request.getSession();
+
+	String uri = request.getRequestURI();
+	if (uri.endsWith("login"))
+	    return true;
+	if (session.getAttribute("username") != null)
+	    return true;
+	return false;
+    }
+
+}
